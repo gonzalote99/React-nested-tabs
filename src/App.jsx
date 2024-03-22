5f165a9fb3a5f46c9fa7e02f12250ff7a04edf7a
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2024-03-22 21:11:58 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css';
+import React from 'react';
+import tabData from './data';
+import TabList from './TabList';
+
+function App() {
+  return <TabList data={tabData} />
+}
+
+export default App;
